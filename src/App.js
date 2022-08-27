34940b53ac432763d8f1760e204805c0a1af1954
MarcellaFox <marcella.fox@outlook.com>
2022-08-27 13:20:43 +1000

    Add GitHub link in footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather Application</div>;
+  return (
+    <div className="App">
+      Weather Application
+      <footer>
+        This project was coded by Marcella Fox and is{" "}
+        <a
+          href="https://github.com/MarcellaFox/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
